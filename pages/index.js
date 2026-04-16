3b4ba59c99d2cb0a59f959f52be62d2b0071beee
Arthur Valente <pedroarthurvalente@gmail.com>
2026-04-16 16:28:46 +0000

    Adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function home(){
+    return <h3>Se nós fomos a primeira civilização do universo, e tudo isso for algo que será amplamente expandido em um futuro muito distante, ainda assim, essa é a melhor vida que alguém, dentre todas as possibilidades de milhares e milhões de séculos, entre qualquer planeta ou galáxia, do uni ou multiverso, poderia ter, simplesmente pelo fato de que apenas nessa vida e nesse momento poderia ser vivido com você.</h3>
+}
+
+export default home;
